b2fbaa063e207521cd9142a1a7b992d2e16f6da2
LeHongTin <lehongtin99@gmail.com>
2018-11-30 15:41:55 +0700

    bai 1+2

---
+++ 02.Bien,KieuDuLieu,ToanTu/TH Chuyển đổi tiền tệ/do.js
@@ -0,0 +1,5 @@
+var viet;
+var dola;
+var dola = parseInt(prompt("Enter the dola"));
+viet=dola*22000;
+document.write("the viet is " + viet);
---
+++ 02.Bien,KieuDuLieu,ToanTu/TH Chuyển đổi tiền tệ/viet.js
@@ -0,0 +1,5 @@
+var viet;
+var dola;
+var viet = parseInt(prompt("Enter the viet"));
+dola=viet/22000;
+document.write("the dola is " + dola);
